fece9d95dee4db746e344c912e749b20290983de
Boris Marinov <boris.marinov99@gmail.com>
2025-07-06 22:50:29 +0300

    Add prisma and create and delete expense

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import prisma from '@/lib/db';
 import ExpensesForm from "@/components/ExpensesForm";
 import ExpensesList from "@/components/ExpensesList";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+    const expenses = await prisma.expense.findMany();
+
     return (
         <div>
             <h1 className="text-3xl font-bold text-white text-center">
@@ -9,7 +12,7 @@ export default function Dashboard() {
             </h1>
 
             <div className="w-full max-w-[600px] mx-auto">
-                <ExpensesList />
+                <ExpensesList expenses={expenses} />
 
                 <ExpensesForm />
             </div>
--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
+'use client';
 
-export default function ExpensesList() {
-    const expenses = [
-        { id: 1, description: 'Groceries', amount: 50 },
-        { id: 2, description: 'Utilities', amount: 100 },
-        { id: 3, description: 'Rent', amount: 1200 },
-    ];
+import { deleteExpense } from "@/actions/actions";
 
+interface ExpensesListProps {
+    expenses: {
+        id: number;
+        description: string;
+        amount: number;
+        createdAt: Date;
+    }[];
+}
+
+export default function ExpensesList({ expenses }: ExpensesListProps) {
     return (
         <ul className='h-[300px] bg-white rounded mt-4 shadow-md'>
             {expenses.map(expense => (
                 <li key={expense.id} className='flex items-center px-4 py-2 border-b'>
                     <p>{expense.description}</p>
                     <p className='ml-auto font-bold mr-[15px]'>${expense.amount.toFixed(2)}</p>
-                    <button className='text-[10px] h-[20px] w-[20px] bg-red-500 text-white rounded hover:bg-red-600'>
+                    <button
+                        onClick={async () => {
+                            await deleteExpense(expense.id);
+                        }}
+                        className='text-[10px] h-[20px] w-[20px] bg-red-500 text-white rounded hover:bg-red-600'
+                    >
                         X
                     </button>
                 </li>
